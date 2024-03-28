1572fe0da4563516e47ec8aac21450791848d49c
adretokhan <adreto.khan@gmail.com>
2024-03-28 12:53:52 +0600

    copyright added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div className="flex justify-center text-gray-300 p-2">@2024 MD. Rafatuzzaman Khan</div>;
+  return (
+    <div className="flex justify-center text-gray-300 p-2">
+      ©2024 MD. Rafatuzzaman Khan
+    </div>
+  );
 };
 
 export default Footer;
